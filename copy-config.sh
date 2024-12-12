e6d7f4363c0f3eef9738bdcfa3ee2846620d8348
arch-yerza <yernazkenesbekov24@gmail.com>
2024-12-12 22:45:56 +0500

    add alacritty

--- copy-config.sh
+++ copy-config.sh
@@ -2,5 +2,9 @@
 
 cp ~/.config/qtile/config.py ~/conf-git/qtile
 cp ~/.config/qtile/autostart.sh ~/conf-git/qtile
+
 cp ~/.config/qtile/my_widget.py ~/conf-git/qtile
 cp ~/.config/nvim/init.vim ~/conf-git/nvim
+
+cp ~/.config/alacritty/alacritty.toml ~/conf-git/alacritty
+cp ~/.config/alacritty/colors.toml ~/conf-git/alacritty
